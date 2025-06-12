0a95f3203c60868842948b65e6a1449541b486a3
kam_cos <cosgun92@gmail.com>
2025-06-12 13:08:25 +0200

    first commit

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -1,15 +1,57 @@
 package com.serenitydojo.playwright;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
+
+import java.util.Arrays;
+
 
 public class ASimplePlaywrightTest {
+    private static Playwright playwright;
+    private static Browser browser;
+    private static BrowserContext browserContext;
+    Page page;
+
+    @BeforeAll
+    public  static void setUpBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions()
+                        .setHeadless(false)
+                        .setArgs(Arrays.asList("--no-sandbox", "--disable-exstensions", "--disable-gpu"))
+        );
+        browserContext = browser.newContext();
+    }
+
+    @BeforeEach
+    public void setUp(){
+        page = browserContext.newPage();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        browser.close();
+        playwright.close();
+    }
 
     @Test
     void shouldShowThePageTitle() {
-        // TODO: Write your first playwright test here
+        page.navigate("https://practicesoftwaretesting.com/");
+        String title = page.title();
+
+        Assertions.assertTrue(title.contains("Practice Software Testing - Toolshop - v5.0"));
+    }
+
+    @Test
+    void shouldSearchByKeyword() {
+
+        page.navigate("https://practicesoftwaretesting.com/");
+        page.locator("input#search-query").fill("pliers");
+        page.locator("button:has-text('Search')").click();
+
+        int matchingSearchResults = page.locator(".card").count();
+
+        Assertions.assertTrue(matchingSearchResults > 0);
+
     }
 }
